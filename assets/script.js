f3f7985c8546bd2517531d7cf7e8844634adf79a
Cyril1743 <75587967+Cyril1743@users.noreply.github.com>
2023-01-15 00:00:30 -0600

    Added function for populating data

--- assets/script.js
+++ assets/script.js
@@ -6,16 +6,14 @@ var searchState = $("#searchState")
 var searchZip = $("#searchZip")
 var cityName = $("#cityName")
 var days = $(".card")
-var currentDay = dayjs("MMM DD, YYYY")
+var currentDay = dayjs().format("MMM DD, YYYY")
 var latAndLon = []
 var cityData = ""
 var weatherData = {}
-
+var currentWeather = $("#cityName")
 searchButton.on("click", function (event) {
     event.preventDefault()
     getLocation()
-    populateData()
-
 })
 
 function getLocation() {
@@ -33,18 +31,57 @@ function getLocation() {
         })
 }
 function getWeather() {
-    fetch('https://api.openweathermap.org/data/3.0/onecall?lat=' + latAndLon[0] + '&lon=' + latAndLon[1] + '&appid=82b6f72e416a643fc9c8ad973faf1aa5')
+    fetch('https://api.openweathermap.org/data/3.0/onecall?lat=' + latAndLon[0] + '&lon=' + latAndLon[1] + '&units=imperial&appid=82b6f72e416a643fc9c8ad973faf1aa5')
         .then(function (reponse) {
             return reponse.json()
         })
         .then(function (data) {
             weatherData = data
             console.log(weatherData)
+            populateData()
         })
 
 }
 function populateData() {
-    
+    currentWeather.append("<h2>" + cityData + "</h2")
+    currentWeather.append("<div>" + currentDay + "</div>")
+    var weatherId = weatherData.current.weather[0].id
+    if (weatherId < 300 && weatherId >= 200) {
+        currentWeather.append("<div> &#127785 </div>")
+    } else if (weatherId < 600 && weatherId >= 300) {
+        currentWeather.append("<div> &#9748 </div>")
+    } else if (weatherId < 700 && weatherId >= 600) {
+        currentWeather.append("<div> &#127784 </div>")
+    } else if (weatherId < 800 && weatherId >= 700) {
+        currentWeather.append("<div> &#127786 </div>")
+    } else if (weatherId == 800) {
+        currentWeather.append("<div> &#127774 </div>")
+    } else {
+        currentWeather.append("<div> &#127780 </div>")
+    }
+    currentWeather.append("<div> Tempature: " + weatherData.current.temp + "F</div>")
+    currentWeather.append("<div> Wind: " + weatherData.current.wind_speed + "MPH</div>")
+    currentWeather.append("<div> Humidity: " + weatherData.current.humidity + "% </div>")
+    $.each(days, function (i, v) {
+        $(this).append("<h4 class='card-title'>" + dayjs().add(i + 1, 'day').format("MMM DD, YYYY") + "</h4>")
+        $(this).append("<div class='card-body'></div>")
+        var icon = weatherData.daily[i].weather[0].id
+        if (icon < 300 && icon >= 200) {
+            $(this).append("<div class='card-text'> &#127785 </div>")
+        } else if (icon < 600 && icon >= 300) {
+            $(this).append("<div class='card-text'> &#9748 </div>")
+        } else if (icon < 700 && icon >= 600) {
+            $(this).append("<div class='card-text'> &#127784 </div>")
+        } else if (icon < 800 && icon >= 700) {
+            $(this).append("<div class='card-text'> &#127786 </div>")
+        } else if (icon == 800) {
+            $(this).append("<div class='card-text'> &#127774 </div>")
+        } else {
+            $(this).append("<div class='card-text'> &#127780 </div>")
+        }
+        $(this).append("<div class='card-text'> High: " + weatherData.daily[i].temp.max + "F</div>")
+        $(this).append("<div class='card-text'> Low:" + weatherData.daily[i].temp.min + "F</div>")
+    })
 }
 
 //setting the elements in the html to data varibles
